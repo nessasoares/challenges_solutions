3a4b8dde949099bfb3e7aa9b2f6136ceae118b55
Vanessa Soares <vanessa.ssoares.vs@gmail.com>
2020-07-03 09:18:30 -0300

    unpaired number

--- codility/unpaired_element.py
+++ codility/unpaired_element.py
@@ -1,7 +1,5 @@
-# you can write to stdout for debugging purposes, e.g.
-# print("this is a debug message")
-
-def solution(A):
+from collections import Counter
+def solution_1(A):
     unpaired = 0
     for number in A:
         counter = A.count(number)
@@ -10,3 +8,9 @@ def solution(A):
             break
     
     return unpaired
+
+def solution_2(A):
+    counter = Counter(A)
+    for item in counter.items():
+        if item[1] % 2 > 0:
+            return item[0]
