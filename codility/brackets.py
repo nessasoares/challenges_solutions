d4f94fcaeca1c138a8e5a7a0707dcb8d8f4c0fd2
Vanessa <vanessa.soares@intelie.com.br>
2020-07-31 23:31:55 -0300

    brackets

--- codility/brackets.py
+++ codility/brackets.py
@@ -1,11 +1,14 @@
 import pytest 
 
-def is_nested(S):
+def old_is_nested(S):
     stack = []
 
     result = 1
     open = ['(', '{', '[']
     close = [')', '}', ']']
+
+    if not S:
+        return 1
 
     if len(S) < 200000:
         for character in S:
@@ -32,6 +35,47 @@ def is_nested(S):
     
     return result
 
+
+def is_nested(S):
+
+    stack = []
+    result = 0
+    # S is empty
+    if not S:
+        return 1
+    if len(S) > 200000:
+        return 0
+
+    open_options = ['{', '(', '[']
+    closed_options =  ['}', ')', ']']
+
+    is_open = lambda e: e in open_options
+    is_closed = lambda e: e in closed_options
+    
+    for element in S:
+        if is_open(element):
+            stack.append(element)
+        elif is_closed(element) and stack:
+            current = element 
+            previous = stack.pop()
+            
+            if open_options.index(previous) == closed_options.index(current):
+                result = 1
+            else:
+                result = 0
+        else:
+            result = 0
+            break
+    
+    if len(stack) > 0:
+        result = 0
+
+    return result
+
+    # S has the form (U)
+
+    # S has the form VW
+
 class TestBrackets():
     def test_nested_examples(self):
         S = "{[()()]}"
@@ -41,8 +85,23 @@ class TestBrackets():
         S = "([)()]"
         assert 0 == is_nested(S)
 
+        S ="()(()()(((()())(()()))"
+        assert 0 == is_nested(S)
+
     def test_not_allowed_string(self):
         S = "([+h)()]"
         assert 0 == is_nested(S)
+
+    def test_cases(self):
+         assert 1 == is_nested("{[()()]}")
+         assert 1 == is_nested("{[(){([])}()]}")
+         assert 1 == is_nested("{}")
+         assert 1 == is_nested("()")
+         assert 1 == is_nested("[]")
+         assert 1 == is_nested("[([][])()]")
+         assert 0 == is_nested("([)()]")
+         assert 0 == is_nested("(]")
+         assert 0 == is_nested("{{{{")
+         assert 0 == is_nested(")))")
 
     
